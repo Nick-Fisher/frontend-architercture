c5a029a18526ee2e6a8c1e3247ac89e6a896e00a
Nick Fisher <manyyearsago@mail.ru>
2023-09-27 13:41:33 +0200

    fix marketing

--- microfrontends/dashboard-example/marketing/config/webpack.prod.js
+++ microfrontends/dashboard-example/marketing/config/webpack.prod.js
@@ -1,16 +1,18 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: {
         './MarketingApp': './src/bootstrap',
       },
